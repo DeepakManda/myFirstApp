f8158e991dd2606bbf420a4ecda5c5f38f28ee29
DeepakManda <49802445+DeepakManda@users.noreply.github.com>
2019-04-19 23:24:42 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',
+ (req, res) => res.send('Hello World!'))
+
+ app.get('/',
+ (req, res) => res.send('welcome to login page'))
+
+
+ app.get('/firstPage',function(req,res)
+ {
+    res.sendFile(__dirname + '/welcome.html');
+ })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

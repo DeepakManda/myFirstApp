1cfae8c0bec61a94ba964bbb9cb5967f8866d2dd
DeepakManda <49802445+DeepakManda@users.noreply.github.com>
2019-04-20 20:50:41 -0700

    UPdated server.js with port

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT || 3000
 
 app.get('/',
  (req, res) => res.send('Hello World!'))
+
+ app.get('/data',
+ (req, res) => res.json({'name':'Deepak','age':'10yrs'}))
 
  app.get('/',
  (req, res) => res.send('welcome to login page'))
